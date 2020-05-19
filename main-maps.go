610948df24cf2e23d0c0a055ebaef87d0e8daa9b
Arlei F. Farnetani Junior <farneetani@gmail.com>
2020-05-19 19:07:09 -0300

    feat: add more example of maps

--- main-maps.go
+++ main-maps.go
@@ -12,12 +12,35 @@ func main() {
 	delete(m,"c")
 	fmt.Println(m["c"])
 
-	_, exists := m["c"]
+	// Num MAP eu tenho 2 propriedades: value, exists (valor/se ele existe)
+	_, exists := m["c"] // como não existe valor por ele ter sido deletado, eu uso um blank identifier _
+	fmt.Println(exists)
+
+	value, exists := m["a"]
 	fmt.Println(exists)
 
 	_, exists2 := m["b"]
 	fmt.Println(exists2)
 
-	value, exists := m["a"]
-	println(value)
+
+	value, exists3 := m["a"]
+	fmt.Println(value)
+	println(exists3)
+
+	value_c, exists := m["c"]
+	fmt.Println(value_c)
+
+	var x = map[string]int{}
+	fmt.Println(x)
+
+	newMap := map[string]int{"x":5, "y":10}
+	fmt.Println(newMap)
+
+	// se o cara existir, entra na primeira condição
+	if value, exists := m["a"]; exists {
+		fmt.Println(value)
+	} else {
+		fmt.Println("ops")
+	}
+
 }
